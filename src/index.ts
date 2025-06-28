fba63283db9e0c39dd4ff803ea6a60513a7c5d59
Pascal Brand <pascal.brand38@gmail.com>
2025-06-28 20:24:22 +0200

    new option: addPlugins
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -17,20 +17,54 @@ export interface AstroLightGalleryType extends HTMLAttributes<"div"> {
    */
   options?: LightGallerySettings,
 
+  addPlugins?: string[],
 }
 
 /** astro components exported, used to create a lightgallery */
 export { default as LightGallery } from './components/LightGallery.astro'
 
+function _textColor(text: string, color: string) {
+  let colorCode: string
+  if      (color === 'FgRed')    { colorCode = '\x1b[31m' }
+  else if (color === 'FgBlue')   { colorCode = '\x1b[34m' }
+  else if (color === 'FgGreen')  { colorCode = '\x1b[32m' }
+  else if (color === 'FgYellow') { colorCode = '\x1b[33m' }
+  else if (color === 'FgCyan')   { colorCode = '\x1b[36m' }
+  else { colorCode = '\x1b[31m' }   // red by default
+  return colorCode + text + '\x1b[0m'
+}
 
-export async function createLightGallery(id: string, options: LightGallerySettings) {
-  const plugins: (new (instance: LightGallery, $LG: LgQuery) => any)[]  = []
+async function _addPlugin(plugins: (new (instance: LightGallery, $LG: LgQuery) => any)[], pluginStr: string) {
+  console.log(_textColor(`astro-lightgallery: add plugin ${pluginStr}`, 'FgGreen'))
+  let plugin = undefined
+  switch (pluginStr) {
+    case 'thumbnail':
+      plugin = await import('lightgallery/plugins/thumbnail')
+      break
+    default:
+      console.log(_textColor(`astro-lightgallery: failed adding unknown plugin ${pluginStr}`, 'FgRed'))
+      break
+  }
+  if (plugin !== undefined) {
+    plugins.push(plugin.default)
+  }
+}
+
+export async function createLightGallery(id: string, options: LightGallerySettings, addPlugins: string[]) {
+  const plugins: (new (instance: LightGallery, $LG: LgQuery) => any)[] = []
   const el = document.getElementById(id)
   if (el) {
-    if (options.thumbnail || options.animateThumb) {
-      const plugin = await import('lightgallery/plugins/thumbnail')
-      plugins.push(plugin.default)
+    // automatic add plugins
+    if ((options.thumbnail!==undefined) || (options.animateThumb!==undefined)) {
+      addPlugins.push('thumbnail')
     }
+
+    // remove duplicates
+    addPlugins = [... new Set(addPlugins)]
+
+    // add plugins
+    await Promise.all(addPlugins.map(async (pluginStr) => await _addPlugin(plugins, pluginStr)))
+
     options.plugins = plugins
 
     ligthGallery(el, options)
