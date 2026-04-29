73ab2be9ce7c17d01c66e0bfa1e567d3cdfff7eb
Pascal Brand <pascal.brand38@gmail.com>
2026-04-29 08:48:28 +0200

    feat: export lightgallery API: types
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

---
+++ lightgallery.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Pascal Brand
+// MIT License
+//
+// export lightgallery API
+// to be used with
+//   import type { LightGallerySettings } from 'astro-lightgallery/lightgallery';
+export type * from 'lightgallery/lg-settings';
